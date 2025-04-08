76a192821ba91851dc4cb09207abf12a5164fda9
sfrdevchit <sfrush4444@gmail.com>
2025-04-08 11:23:18 +0800

    Create hwid_check.php

---
+++ hwid_check.php
@@ -0,0 +1,20 @@
+<?php
+// List of valid HWIDs (add these manually as users purchase your loader)
+$valid_hwids = [
+    "examplehwid1234567890abcdef12345678", // Replace with real HWIDs
+    "anotherhwidabcdef1234567890abcdef12"
+];
+
+// Get HWID from the GET request sent by the loader
+$hwid = isset($_GET['hwid']) ? $_GET['hwid'] : '';
+
+// Set response header to JSON
+header('Content-Type: application/json');
+
+// Check if the HWID is in the valid list
+if (in_array($hwid, $valid_hwids)) {
+    echo json_encode(['status' => 'valid']);
+} else {
+    echo json_encode(['status' => 'invalid']);
+}
+?>
